9ffb32ded0b1509808354e61890086b45251ea53
chandan shetty <chandanshetty01@gmail.com>
2014-06-08 23:40:24 +0800

    Supported kite animation for balloon too

---
+++ KachiKachi/Elements/TTBalloon.h
@@ -0,0 +1,13 @@
+//
+//  TTBalloon.h
+//  KachiKachi
+//
+//  Created by Chandan on 08/08/2013.
+//  Copyright (c) 2014 Chanddan. All rights reserved.
+//
+
+#import "TTBase.h"
+
+@interface TTBalloon : TTBase
+
+@end
---
+++ KachiKachi/Elements/TTBalloon.m
@@ -0,0 +1,46 @@
+//
+//  TTBalloon.m
+//  KachiKachi
+//
+//  Created by Chandan on 08/08/2013.
+//  Copyright (c) 2014 Chanddan. All rights reserved.
+//
+
+#import "TTBalloon.h"
+
+@implementation TTBalloon
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+-(void)showAnimation:(completionBlk)completionBlk
+{
+    [UIView animateWithDuration:0.8
+                          delay:0.2
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         CGRect frame = self.frame;
+                         frame.origin.y = -200;
+                         self.frame = frame;
+                     } completion:^(BOOL finished) {
+                         completionBlk(finished);
+                     }];
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
