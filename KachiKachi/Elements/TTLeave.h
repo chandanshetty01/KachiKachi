606f86d01fb4ee6eb00544c74cf764131b369e6e
chandan shetty <chandanshetty01@gmail.com>
2014-06-19 22:42:39 +0800

    Supported animation for leaves

---
+++ KachiKachi/Elements/TTLeave.h
@@ -0,0 +1,14 @@
+//
+//  TTLeave.h
+//  KachiKachi
+//
+//  Created by Chandan on 08/08/2013.
+//  Copyright (c) 2014 Chanddan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TTBase.h"
+
+@interface TTLeave : TTBase
+
+@end
---
+++ KachiKachi/Elements/TTLeave.m
@@ -0,0 +1,27 @@
+//
+//  TTLeave.m
+//  KachiKachi
+//
+//  Created by Chandan on 08/08/2013.
+//  Copyright (c) 2014 Chanddan. All rights reserved.
+//
+
+#import "TTLeave.h"
+
+@implementation TTLeave
+
+-(void)showAnimation:(completionBlk)completionBlk
+{
+    [UIView animateWithDuration:0.8
+                          delay:0.2
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         CGRect frame = self.frame;
+                         frame.origin.y = 1000;
+                         self.frame = frame;
+                     } completion:^(BOOL finished) {
+                         completionBlk(finished);
+                     }];
+}
+
+@end
